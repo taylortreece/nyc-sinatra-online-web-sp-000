0deb945a93cc9e00d74ea42672f7327715b23278
Taylor Treece <taylor.treece@protonmail.com>
2021-03-17 09:20:56 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -16,12 +16,12 @@ class FiguresController < ApplicationController
     @figure=Figure.create(params[:figure])
     @figure.title_ids = params[:figure][:title_ids]
     @figure.landmark_ids = params[:figure][:landmark_ids]
-    
-      if params[:title][:name] != ""
+
+      if !params[:title][:name].empty?
         @figure.title_ids << Title.find_or_create_by(name: params[:title][:name]).save.id
       end
 
-      if params[:landmark][:name] != ""
+      if !params[:landmark][:name].empty?
         @figure.landmark_ids << Landmark.find_or_create_by(name: params[:landmark][:name]).save.id
       end
 
